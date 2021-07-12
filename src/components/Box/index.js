c0afacfdd80890afe60f4f06e8f4320e0a77b631
julianjgn <contato@juliannunes.com.br>
2021-07-12 15:26:29 -0300

    Init Alurakut

---
+++ src/components/Box/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// sempre que for um componente, começa maiúsculo
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+  padding: 16px;
+`; 
+
+export default Box;
